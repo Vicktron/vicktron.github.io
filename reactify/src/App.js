b0a4968a74fad3b2fca67b6063f59c25e0591bc7
Elishaokon13 <eliscodavid@gmail.com>
2022-06-16 06:08:42 -0700

    react

---
+++ reactify/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './pages/Home'; 
+import Deposit from './pages/Deposit';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Deposit />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactify/src/pages/404.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+class error extends React.Component {
+  render() {
+    return (
+      <div>
+        <section
+      class="error section__space bg__img"
+      data-background="./assets/images/error-bg.png"
+    >
+      <div class="container">
+        <div class="error__area text-center">
+          <img
+            src="assets/images/404.png"
+            class="section__header__space"
+            alt="Go Back To Home"
+          />
+          <div class="error__content">
+            <h3 class="content__space">Page Not Found</h3>
+            <h4 class="cta__space">Oops.. looks like you got lost</h4>
+            <a href="index.html" class="button">Go Back Home</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+
+    
+    <script src="assets/vendors/jquery/jquery.min.js"></script>
+    
+    <script src="assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+    
+    <script src="assets/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+    
+    <script src="assets/vendors/magnific-popup/js/jquery.magnific-popup.min.js"></script>
+    
+    <script src="assets/vendors/slick/js/slick.js"></script>
+    
+    <script src="assets/vendors/shuffle/shuffle.min.js"></script>
+
+    
+
+    
+    <script src="assets/js/plugin.js"></script>
+    
+    <script src="assets/js/main.js"></script>
+  
+    </div>
+    );
+  }
+}
+
+export default error
