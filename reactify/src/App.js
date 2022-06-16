96a042362cd4712bf26d9e0c17a1a1c2b2140661
Elishaokon13 <eliscodavid@gmail.com>
2022-06-16 06:14:44 -0700

    EDITED INDEX

--- reactify/src/App.js
+++ reactify/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './pages/Home'; 
 import Deposit from './pages/Deposit';
+import dashboardLottery from './pages/DashboardLottery';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <Deposit />
+      <dashboardLottery />
     </div>
   );
 }
